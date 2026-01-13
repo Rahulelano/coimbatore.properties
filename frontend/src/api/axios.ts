ee5ceaa4be34ff913d9fad67734674dffd3019e5
Rahulelano <132381959+Rahulelano@users.noreply.github.com>
2026-01-13 18:24:25 +0530

    Update Axios base URL for local development

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-    baseURL: import.meta.env.MODE === 'production' ? '/api' : 'http://localhost:5001/api',
+    baseURL: import.meta.env.MODE === 'production' ? '/api' : 'http://localhost:6005/api',
     headers: {
         'Content-Type': 'application/json',
     },
